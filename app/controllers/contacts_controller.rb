7d5ae53e95810a7e23ca39ef41bc0a9be1fdae75
guillermo varela <guillovarela16@gmail.com>
2019-07-23 13:09:14 -0300

    chage the redirect in controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,6 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
-      redirect_to companys_path, notice: 'Message sent successfully'
     else
       flash.now[:error] = 'Cannot send message'
       render :new
