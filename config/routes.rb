5fb49d15eca71effc9ae59ed737c5a2e280355be
guillermo varela <guillovarela16@gmail.com>
2019-07-23 10:28:47 -0300

    finish links and new contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :companys, only: [:index]
   resources :publics, only: [:index]
-  resoruces :contacts, only: [:new, :create]
+  resources :contacts, only: [:new, :create]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
