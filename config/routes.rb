b48d0585b005b779ef6976db41ea467f62bbdad4
guillermo varela <guillovarela16@gmail.com>
2020-03-15 14:18:37 -0300

    in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :companys, only: [:index]
-  resources :publics, only: [:index]
+
+  get '/about',to: 'pages#about'
+
   resources :contacts, only: [:new, :create]
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
